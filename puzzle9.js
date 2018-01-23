85c79199b05c9c4ed035e8a38d6971d3d13ba5a6
Tina Ye <tinaye@berkeley.edu>
2018-01-23 00:00:47 -0800

    Create puzzle9.js

---
+++ puzzle9.js
@@ -0,0 +1,12 @@
+function repeat(direction, max) {
+  var count = 0;
+  while (count < max) {
+    direction;
+    count = count + 1;
+  }
+}
+
+repeat(down, 5);
+repeat(right, 9);
+repeat(up, 5);
+repeat(left, 9);
