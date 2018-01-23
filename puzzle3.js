951360555562241b4b3bcfb0f54ac655344924bc
Tina Ye <tinaye@berkeley.edu>
2018-01-22 23:36:18 -0800

    Create puzzle3.js

---
+++ puzzle3.js
@@ -0,0 +1,15 @@
+function repeat(direction, max) {
+  var count = 0;
+  while (count < max) {
+    direction;
+    count = count + 1;
+  }
+}
+
+repeat(down, 2);
+var myColor = getColor();
+repeat(down, 3);
+repeat(right, 3);
+setColor(myColor);
+right;
+up;
