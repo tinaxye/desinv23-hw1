e2b40bf1dc1a554cd30c0f494382f79e8480e527
Tina Ye <tinaye@berkeley.edu>
2018-01-22 23:58:36 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,22 @@
+function repeat(direction, max) {
+  var count = 0;
+  while (count < max) {
+    direction;
+    count = count + 1;
+  }
+  right;
+}
+
+repeat(down, 8);
+right;
+
+repeat(up, 7);
+right;
+
+repeat(down, 7);
+right;
+
+repeat(up, 7);
+right;
+
+repeat(down, 7);
