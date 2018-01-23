17f58f99e08c283c2a815f95acf0535506ea1709
Tina Ye <tinaye@berkeley.edu>
2018-01-22 23:52:05 -0800

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,26 @@
+right;
+var firstColor = getColor();
+
+if (firstColor == "blue") {
+  down;
+  down;
+  down;
+} else {
+  up;
+  up;
+  up;
+}
+
+if (getColor() == "blue") {
+  right;
+} else {
+  left;
+}
+
+if (firstColor == "blue") {
+  down;
+} else {
+  up;
+}
+
+  
