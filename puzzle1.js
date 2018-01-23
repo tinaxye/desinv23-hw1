ff959ca01af20a6891aa0f2801e36a8989fca77f
Tina Ye <tinaye@berkeley.edu>
2018-01-22 23:27:39 -0800

    Create puzzle1.js

---
+++ puzzle1.js
@@ -0,0 +1,24 @@
+var count = 0;
+
+while (count < 7) {
+  down();
+  count = count + 1;
+}
+
+count = 0;
+while (count < 5) {
+  right();
+  count = count + 1;
+}
+
+count = 0;
+while (count < 2) {
+  up();
+  count = count + 1;
+}
+
+count = 0;
+while (count < 2) {
+  right();
+  count = count + 1;
+ ]
