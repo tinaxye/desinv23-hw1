a39cc88d0d3a066bfe794f2c215ab88e7db7e9cf
Tina Ye <tinaye@berkeley.edu>
2018-01-22 23:45:59 -0800

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,18 @@
+function moveThroughObstacle() {
+  right;
+  right;
+  if (getColor == "blue") {
+    down;
+  } else {
+    up;
+  }
+}
+
+var count = 0;
+while (count < 3) {
+  moveThroughObstacle();
+  count = count + 1;
+}
+right;
+right;
+right;
