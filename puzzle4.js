4a4807485ffd0c28fc979c5bf1ee5d2d210bdf01
Tina Ye <tinaye@berkeley.edu>
2018-01-22 23:42:01 -0800

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,21 @@
+function repeat(direction, max) {
+  var count = 0;
+  while (count < max) {
+    direction;
+    count = count + 1;
+  }
+}
+
+function changeAndCollect() {
+  right;
+  var myColor = getColor();
+  down;
+  right;
+  setColor(myColor);
+  right;
+}
+
+repeat(down, 4);
+changeAndCollect();
+up;
+changeAndCollect();
